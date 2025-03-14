9a85c4fcb87261bdb0fa36370197dc5aa4a0adc4
Austin76-cs <poptart4000@gmail.com>
2025-03-13 17:19:59 -0700

    Login change

---
+++ ScriptsActivate.ps1
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+      568     567     568      13284  pty0      197609 15:54:06 /usr/bin/bash
+      567       1     567       1436  ?         197609 15:54:06 /usr/bin/mintty
+      645     568     645       4088  pty0      197609 16:05:02 /usr/bin/PS
